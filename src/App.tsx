837ede9d60e3a4e1bc3d4fde6f70b81e68a0d97e
6Neel9 <nilmandal300@gmail.com>
2025-08-16 17:04:18 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react'
+import { useMemo, useState } from 'react'
 import CalendarGrid from './components/CalenderGrid'
 import FiltersPanel, { type Filters, applyFilters } from './components/FiltersPanel'
 import Modal from './components/Modal'
